0acdbf28887c5a78bbccf4098987e157f9701026
Wcollard <Wcollard@yahoo.com>
2022-01-22 18:37:57 -0500

    Create 9_Printing_Printing.py

---
+++ 9_Printing_Printing.py
@@ -0,0 +1,15 @@
+# Here is some new strange stuff, remember type it exactly.
+
+days= " Mon Tue Wed Thu Fri Sat Sun"
+months= "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print ("Here are the days: ", days)
+print ("Here are the months: ", months)
+
+print ("""
+THere is something going on here
+With the Three double-quotes.
+We'll be able to type as much asd wel like.
+Even 4 lines if we want, or 5, or 6. 
+"""
+)
